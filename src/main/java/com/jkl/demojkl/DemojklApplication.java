77900008497a1e42929be828ce12941c9f00cc36
INT\klawej <juergen.klawe@msg.group>
2025-09-01 12:33:34 +0200

    first changes

---
+++ src/main/java/com/jkl/demojkl/DemojklApplication.java
@@ -0,0 +1,13 @@
+package com.jkl.demojkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojklApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojklApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkl/demojkl/DemojklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkl.demojkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojklApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
